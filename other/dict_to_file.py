50ccf288d511084937e52e861325ad507f99da94
jonas <JONAS_402@msn.com>
2016-05-05 18:19:06 +0100

    v1.1 - Cleaned and refined, noticed it only works with one album in the directory otherwise it puts all tracks in same album?

--- other/dict_to_file.py
+++ other/dict_to_file.py
@@ -1,70 +1,40 @@
-import os
-dict = {}
-str = '01 track1 with lots of spaces.mp3', '02 track2-with-hyphens.mp3', '03 track3_with_under_scores.mp3', '04 track4.mp3', '05 track5.mp3', '06 track6.mp3'
-dir = '/home/jonas/musictest'
-temp = {}
-albums = {}
-album1 = []
-for root, dirs, files in os.walk(dir, topdown=False):
+# Music Crawler (only works with single albums in artist directory)
+#   V1.1
 
-    # print(files)
-    for name in files:
-        # print(name)
-        x = root.split('/')
-        rev = x[::-1]
-        album, band, *shit = rev
-        if name.endswith('.mp3'):
-            # print('root', root)
-            # print('dirs', dirs)
-            # print("artist {0}, band {1}".format(album, band))
+def music_crawl(directory):
+    import os
+    holding_dict = {}
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for track_name_and_number in files:
+            root_split = root.split('/')
+            root_split_reversed = root_split[::-1]
+            album, band, *waste = root_split_reversed
 
-            xz = name.split(" ", 1)
-            print('zx =', xz)
-            z = (xz[0])
-            y = (xz[1])
-            temp[z] = y
-    literal = repr(temp)
-    album1.append(" '")
-    album1.append(album)
-    album1.append("': ")
-    album1.append(literal)
-    empty_string = ''.join(album1)
-    # if album not in band dir then print band name on new line with collection
-    collection = []
-    collection.append(band)
-    collection.append(" = [")
-    collection.append(empty_string)
-    collection.append("]\n")
-    collection1 = ''.join(collection)
-    print('empty_string = ', empty_string)
-# print('literal ', literal)
-    print('album1 = ', album1)
-# print(album + " = " + literal)
-with open("my_dict_test.py", "w") as f:
-    print(collection1, file=f)
+            if track_name_and_number.endswith('.mp3'):
+                # print('root', root)
+                print("Artist: {1}, Album: {0}   Track name: {2}".format(album, band, track_name_and_number))
+                track_name_and_number_split = track_name_and_number.split(" ", 1)
+                track_number = (track_name_and_number_split[0])
+                track_name = (track_name_and_number_split[1])
+                holding_dict[track_number] = track_name
+            elif track_name_and_number.endswith('.m4a'):
+                print("Artist: {1}, Album: {0}   Track name: {2}".format(album, band, track_name_and_number))
+                track_name_and_number_split = track_name_and_number.split(" ", 1)
+                track_number = (track_name_and_number_split[0])
+                track_name = (track_name_and_number_split[1])
+                holding_dict[track_number] = track_name
+
+        literal = repr(holding_dict)
+        full_album = [" '", album, "', ", literal]
+        empty_string = ''.join(full_album)
+        # if album not in band dir then print band name on new line with collection
+        collection = [band, " = [", empty_string, "]\n"]
+        collection1 = ''.join(collection)
+
+    with open("my_dict_test.py", "w") as f:
+        print(collection1, file=f)
+
+dir1 = '/home/jonas/musictest'
+music_crawl(dir1)
 
 
-'''
-for i in str:
-    x = i.split(" ", 1)
-    print(x)
-    z = (x[0])
-    y = (x[1])
-    dict[z] = y
-
-print(dict)
-literal = repr(dict)
-    x = i.split(" ", 1)
-    print(x)
-    z = (x[0])
-    y = (x[1])
-    dict[z] = y
-
-print(dict)
-literal = repr(dict)
-
-#
-
-print()
-print("dict = ", literal)
-'''
